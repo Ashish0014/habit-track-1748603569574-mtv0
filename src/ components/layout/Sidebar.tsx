b1b3d21eb38f5d43dff4eb1ede58f924be28f89e
Ashish0014 <138017791+Ashish0014@users.noreply.github.com>
2025-05-30 16:43:14 +0530

    Add src/ components/layout/Sidebar.tsx

---
+++ src/ components/layout/Sidebar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Sidebar: React.FC = () => {
+  return (
+    <aside className="w-64 bg-gray-900 text-white">
+      {/* Collapsible sidebar */}
+      {/* Active route highlighting */}
+      {/* Icon + text navigation */}
+      {/* Mobile responsive */}
+    </aside>
+  );
+};
+
+export default Sidebar;
