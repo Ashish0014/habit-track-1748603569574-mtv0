ade005021640eb33768f4efe6f3475aad15be1da
Ashish0014 <138017791+Ashish0014@users.noreply.github.com>
2025-05-30 16:43:12 +0530

    Add src/ components/layout/Header.tsx

---
+++ src/ components/layout/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
+      {/* Navigation links */}
+      {/* User profile dropdown */}
+      {/* Theme toggle */}
+      {/* Responsive menu */}
+    </header>
+  );
+};
+
+export default Header;
