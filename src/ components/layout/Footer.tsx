8457570334c106f5bd80830c437656e1e7de20d6
Ashish0014 <138017791+Ashish0014@users.noreply.github.com>
2025-05-30 16:43:16 +0530

    Add src/ components/layout/Footer.tsx

---
+++ src/ components/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4 flex justify-between items-center">
+      {/* Social links */}
+      {/* Newsletter signup */}
+      {/* Copyright info */}
+    </footer>
+  );
+};
+
+export default Footer;
